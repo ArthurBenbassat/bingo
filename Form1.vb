0a605dc17186e0b590af8d9b35a07ff6dcd3c5c5
ViewSonic <45071521+ArthurBenbassat@users.noreply.github.com>
2019-02-26 11:45:24 +0100

    Add files via upload

--- Form1.vb
+++ Form1.vb
@@ -1,31 +1,20 @@
 ﻿Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim getallen(5) As Integer
-        Dim gemiddelde As Decimal
-        Dim tmp As String = ""
+        Dim kleur() As String = {"rood", "oranje", "geel", "groen", "blauw", "indigo", "violet"}
+        Dim getal As Integer = "1"
 
-        For teller As Integer = 0 To getallen.GetUpperBound(0)
-            Try
-                getallen(teller) = InputBox("Geef een getal: ")
-            Catch ex As Exception
-                MessageBox.Show("Voer een getal in")
-                teller += 1
-            End Try
 
+
+        For i As Integer = 0 To 5
+            getal = willgetal(0, 6)
+            MessageBox.Show("U favoriete kleur is :" & kleur(getal))
         Next
 
-        For teller As Integer = 0 To 5
-
-            tmp &= getallen(teller) & Space(1)
-
-        Next
-
-        For teller As Integer = 0 To 5
-            gemiddelde += getallen(teller)
-        Next
-
-        gemiddelde = gemiddelde / 6
-
-        MessageBox.Show(tmp & vbCrLf & "Dit is het gemiddelde is " & gemiddelde)
     End Sub
+    Public Function willgetal(ByVal ondergrens As Integer, ByVal bovengrens As Integer) As Integer
+        Dim will As Integer
+        Randomize()
+        will = CInt(Int((bovengrens - ondergrens + 1) * Rnd() + ondergrens))
+        Return will
+    End Function
 End Class
